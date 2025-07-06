c546a2927db4a103f8bd44afd3c9bf6ad18b34dd
skvadda <157056655+skvadda@users.noreply.github.com>
2025-07-06 11:23:54 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 build: {
-  outDir: "dist"
+  outDir: "dist/public",
+  emptyOutDir: True,
 }
 export default defineConfig({
   plugins: [
