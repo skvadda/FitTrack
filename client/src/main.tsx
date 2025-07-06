2d6b76557ec60289e9cc47f069d8955b11c967da
skvadda <157056655+skvadda@users.noreply.github.com>
2025-07-05 21:10:26 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { Analytics } from '@vercel/analytics/next';
-{children}
-<Analytics />
+
 createRoot(document.getElementById("root")!).render(<App />);
